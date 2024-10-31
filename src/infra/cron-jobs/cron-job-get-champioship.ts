429102319a222d0782b662a1615e5d094a4af7be
Romeu Sorionaet <romeuindexjs@gmail.com>
2024-10-31 11:21:25 -0300

    chore: improved config in cron jobs warrant operation

--- src/infra/cron-jobs/cron-job-get-champioship.ts
+++ src/infra/cron-jobs/cron-job-get-champioship.ts
@@ -9,27 +9,39 @@ import { upsertMatchData } from "../database/prisma/match/upsert-match";
 import cron from "node-cron";
 
 export async function cronJobGetChampionshipMatches() {
-  try {
-    const responseChampionship = await fetchChampionshipData();
-    const dataChampionship = championshipSchema.parse(responseChampionship);
-    const { id: championshipId } = await upsertChampionship(dataChampionship);
+  const now = new Date();
 
-    const responseDataMatches = await fetchChampionshipMatches();
-    const dataMatchesList = matchesListSchema.parse(responseDataMatches);
+  if (now.getDate() === 10 && now.getMonth() >= 3 && now.getMonth() <= 10) {
+    try {
+      const responseChampionship = await fetchChampionshipData();
+      const dataChampionship = championshipSchema.parse(responseChampionship);
+      const { id: championshipId } = await upsertChampionship(dataChampionship);
 
-    if (!dataMatchesList.partidas || !dataMatchesList.partidas["fase-unica"]) {
-      return console.log({
-        error_code: "INVALID_DATA_STRUCTURE",
-        message_error: "Estrutura de dados inesperada da API.",
-      });
+      const responseDataMatches = await fetchChampionshipMatches();
+      const dataMatchesList = matchesListSchema.parse(responseDataMatches);
+
+      if (
+        !dataMatchesList.partidas ||
+        !dataMatchesList.partidas["fase-unica"]
+      ) {
+        return console.log({
+          error_code: "INVALID_DATA_STRUCTURE",
+          message_error: "Estrutura de dados inesperada da API.",
+        });
+      }
+
+      const matchesByRounds = dataMatchesList.partidas["fase-unica"];
+      const allMatches = Object.values(matchesByRounds).flat();
+
+      await upsertMatchData(allMatches, championshipId);
+
+      console.log(
+        "Cron-Job Get Championship Matches, foi execultado com sucesso: ",
+        now,
+      );
+    } catch (error) {
+      console.error("Erro ao monitorar partidas:", error);
     }
-
-    const matchesByRounds = dataMatchesList.partidas["fase-unica"];
-    const allMatches = Object.values(matchesByRounds).flat();
-
-    await upsertMatchData(allMatches, championshipId);
-  } catch (error) {
-    console.error("Erro ao monitorar partidas:", error);
   }
 }
 
--- src/infra/cron-jobs/cron-job-update-matches.ts
+++ src/infra/cron-jobs/cron-job-update-matches.ts
@@ -5,29 +5,43 @@ import { fetchMatch } from "../services/soccer-api";
 import cron from "node-cron";
 
 export async function cronJobUpdateMatches() {
-  try {
-    const matchIdList = await findMatchesByNearestDateAndTime();
+  const now = new Date();
 
-    if (matchIdList.length === 0) {
-      return;
-    }
+  if (
+    now.getHours() === 1 &&
+    now.getMinutes() === 0 &&
+    now.getMonth() >= 3 &&
+    now.getMonth() <= 11
+  ) {
+    try {
+      const matchIdList = await findMatchesByNearestDateAndTime();
 
-    for (const match of matchIdList) {
-      try {
-        const matchData = await fetchMatch({ matchId: match.id });
-
-        const data = matchDataSchema.parse(matchData);
-
-        await Promise.all([
-          saveMatchDetails({ type: "mandante", matchData: data }),
-          saveMatchDetails({ type: "visitante", matchData: data }),
-        ]);
-      } catch (error) {
-        console.error(`Erro ao buscar dados da partida`, error);
+      if (matchIdList.length === 0) {
+        return;
       }
+
+      for (const match of matchIdList) {
+        try {
+          const matchData = await fetchMatch({ matchId: match.id });
+
+          const data = matchDataSchema.parse(matchData);
+
+          await Promise.all([
+            saveMatchDetails({ type: "mandante", matchData: data }),
+            saveMatchDetails({ type: "visitante", matchData: data }),
+          ]);
+
+          console.log(
+            "Cron-Job Update Matches foi execultado com sucesso: ",
+            now,
+          );
+        } catch (error) {
+          console.error(`Erro ao buscar dados da partida`, error);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao monitorar partidas:", error);
     }
-  } catch (error) {
-    console.error("Erro ao monitorar partidas:", error);
   }
 }
 
